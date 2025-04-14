264cdbd5a17794cc97a23170906b292613d26a98
santiago803 <santiago.londono13@udea.edu.co>
2025-04-14 17:19:22 -0500

    Modulo de las funciones

---
+++ Desafio/Funciones.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+char o = "245";
+unsigned char mascara_desplazamiento(bool desplazamiento){
+
+}
+
+
+void funcion_rotacion(int img,unsigned short int n,bool desplazamiento){
+    /*Esta función recibe un arreglo con valores RGB de la imagen (img)y recibe la cantidad de bits que se roto(n).
+    La función realiza una rotación de bits en los valores de cada píxel,
+    ya sea rotando los bits hacia la derecha o hacia la izquierda una cantidad de i veces y retorna la imagen tranformada. */
+    uint64_t mask = "10011";
+
+
+    for(int i = 0; i < size(img) ; i ++){
+        for(unsigned short int j = 0; j<n ;j++){
+            if (desplazamiento){
+                img[i] >> b ;
+            }else{
+
+            }
+
+        }
+    }
+
+}
